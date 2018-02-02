30feb6c26abbe2150013c0f97c3820aa1983f94b
g-hyoga <hyoga0216@gmail.com>
2018-02-02 20:42:07 +0900

    add result.md and exchange(fix) name

--- src/13.2/observ.c
+++ src/13.2/observ.c
@@ -1,37 +1,24 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 
-int main() {
-  int aAddress = shmget (IPC_PRIVATE, sizeof(int), IPC_CREAT|0666);
-  int bAddress = shmget (IPC_PRIVATE, sizeof(int), IPC_CREAT|0666);
-
-  int *a = (int *) shmat (aAddress, (void *) 0, 0);
-  int *b = (int *) shmat (bAddress, (void *) 0, 0);
-
-  if (aAddress == -1 || bAddress == -1) {
-    printf("shared memory error");
+int main (int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("input A Address and B Address on commandline arg\n");
     return -1;
   }
 
-  printf("A Address: %d\n", aAddress);
-  printf("B Address: %d\n", bAddress);
+  int aAddress = atoi (argv[1]);
+  int bAddress = atoi (argv[2]);
 
-  *a = 100;
-  *b = 0;
+  int *a = (int *) shmat (aAddress, 0, 0);
+  int *b = (int *) shmat (bAddress, 0, 0);
 
-  while(1) {
-    printf("(a, b) = (%d, %d)\n", *a, *b);
-
-    *a = *a - 1;
-    *b = *b + 1;
-
+  while(1){
+    printf("a + b = %d\n", *a + *b);
     sleep(1);
-    if (*a == 0 && *b == 100) {
-      break;
-    }
   }
 }
