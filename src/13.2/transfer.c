1b9effec1eec3b00ecf5aa5a4bc6c2c0797be2f6
g-hyoga <hyoga0216@gmail.com>
2018-02-02 20:35:57 +0900

    add transfer.c

--- src/13.2/transfer.c
+++ src/13.2/transfer.c
@@ -6,5 +6,19 @@
 #include <string.h> 
 
 int main (int argc, char *argv[]) {
- 
+  if (argc != 3) {
+    printf("input A Address and B Address on commandline arg\n");
+    return -1;
+  }
+
+  int aAddress = atoi (argv[1]);
+  int bAddress = atoi (argv[2]);
+
+  int *a = (int *) shmat (aAddress, 0, 0);
+  int *b = (int *) shmat (bAddress, 0, 0);
+
+  while(1){
+    printf("a + b = %d\n", *a + *b);
+    sleep(1);
+  }
 }
